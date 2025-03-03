7bd0e9934a364eaca927de7ea9730db5741db642
Bee Alkayde <hanclaid@gmail.com>
2025-03-04 04:15:10 +0800

    Changes in supaBaseClient.js

--- frontend/src/api/supabaseClient.js
+++ frontend/src/api/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://vpzmsuldpbfxmgqllwbc.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwem1zdWxkcGJmeG1ncWxsd2JjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDQyNDY5MywiZXhwIjoyMDU2MDAwNjkzfQ.Ghjquhm59sqIs3ITtrT-c5uVPggIIegKicph7bDAGjk"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwem1zdWxkcGJmeG1ncWxsd2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MjQ2OTMsImV4cCI6MjA1NjAwMDY5M30.oUuQ49LPcWjmrXITS18_EQg4-STC8cA4ETfpNIMkm0w"
 // ✅ Check if API keys exist
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("🚨 Missing Supabase API credentials!");
